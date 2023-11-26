b0ce88c3ce678d465da0c357221a8466dc7e1a22
Michael Busser <m.busser@bbstghs.de>
2023-11-26 14:04:51 +0100

    Defining the pins used by hardare. Setting up a test programm to see, if the hardware is working correctly. All I2C devices are responding.

---
+++ src/ClimateLogger/include/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+#define RXD2 16
+#define TXD2 17
+
+#define LED_STABLE      26          //
+#define LED_NOISE       27          //
+#define LED_LOADED      18          //
+#define LED_PRINT       19          //
+#define LED_SPEED       23
+
+#define SW_CONFIG       32          //
+#define SW_STABLE       15          //
+#define SW_NOISE         2          //
+#define SW_LOADED        0          //
+#define SW_PRINT         4          //
+#define SW_SPEED         5          //
+
+#define MONITOR_SPEED    115200
+#define SDA              21
+#define SCL              22
+
+
+
+#endif
---
+++ src/ClimateLogger/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+void Scanner ()
+{
+  Serial.println ();
+  Serial.println ("I2C scanner. Scanning ...");
+  byte count = 0;
+
+  Wire.begin();
+  for (byte i = 8; i < 120; i++)
+  {
+    Wire.beginTransmission (i);          // Begin I2C transmission Address (i)
+    if (Wire.endTransmission () == 0)  // Receive 0 = success (ACK response) 
+    {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);     // PCF8574 7 bit address
+      Serial.println (")");
+      count++;
+    }
+  }
+  Serial.print ("Found ");      
+  Serial.print (count, DEC);        // numbers of devices
+  Serial.println (" device(s).");
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin (115200);  
+  Wire.begin ( SDA, SCL );   // sda= GPIO_21 /scl= GPIO_22
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Scanner ();
+  delay (500);
+
+}
+
