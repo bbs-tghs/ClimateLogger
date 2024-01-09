4f4ecbabd890feba3952e4674f0858c575f2ef4a
Ella <julian2003sch@gmail.com>
2024-01-09 11:42:45 +0100

    Added Liberies for seonsors and reformated the code

--- src/ClimateLogger/src/main.cpp
+++ src/ClimateLogger/src/main.cpp
@@ -1,42 +1,46 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_BME680.h>
+#include <Adafruit_CCS811.h>
 
-void Scanner ()
+void scanner ()
 {
-  Serial.println ();
-  Serial.println ("I2C scanner. Scanning ...");
+  Serial.println();
+  Serial.println("I2C scanner. Scanning ...");
   byte count = 0;
 
   Wire.begin();
   for (byte i = 8; i < 120; i++)
   {
-    Wire.beginTransmission (i);          // Begin I2C transmission Address (i)
-    if (Wire.endTransmission () == 0)  // Receive 0 = success (ACK response) 
+    Wire.beginTransmission(i);          // Begin I2C transmission Address (i)
+    if (Wire.endTransmission() == 0)  // Receive 0 = success (ACK response) 
     {
-      Serial.print ("Found address: ");
-      Serial.print (i, DEC);
-      Serial.print (" (0x");
-      Serial.print (i, HEX);     // PCF8574 7 bit address
-      Serial.println (")");
+      Serial.print("Found address: ");
+      Serial.print(i, DEC);
+      Serial.print(" (0x");
+      Serial.print(i, HEX);     // PCF8574 7 bit address
+      Serial.println(")");
       count++;
     }
   }
-  Serial.print ("Found ");      
-  Serial.print (count, DEC);        // numbers of devices
-  Serial.println (" device(s).");
+  Serial.print("Found ");      
+  Serial.print(count, DEC);        // numbers of devices
+  Serial.println(" device(s).");
 }
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin (115200);  
-  Wire.begin ( SDA, SCL );   // sda= GPIO_21 /scl= GPIO_22
+  Serial.begin(115200);  
+  Wire.begin( SDA, SCL );   // sda= GPIO_21 /scl= GPIO_22
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Scanner ();
-  delay (500);
+  scanner();
+  delay(500);
 
 }
 
